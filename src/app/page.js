0bd7c90fc3f0aa290ebcee4adb8333a118a5f553
Nazrul144 <nazrulislam.cse28@gmail.com>
2024-10-14 21:16:33 +0600

    First Project

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>This is Next.js Bro. Now It's built</h1>
+      <h1>This is Next.js Bro.</h1>
     </div>
   );
 }
